ffaabe6fc12b705b670d18d5460f5e666d0b4b7c
Stainley Lebron <saiyamans@gmail.com>
2022-11-05 14:09:59 -0400

    Initial Commit

---
+++ ManagmentFinaliOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ManagmentFinaliOS
+//
+//  Created by Stainley A Lebron R on 2022-11-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
